d47423a8d3ca41c6d2486036be06e941a27313a4
Yue Zou <yz3936@columbia.edu>
2020-10-21 07:21:35 +0000

    change format of html and make some change of urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.conf.urls import url
 
-#app_name = 'sightings'
+app_name = 'sightings'
 urlpatterns = [
 		path('',views.sightings,name="sightings"),
                 path('add/',views.add,name="add"),
